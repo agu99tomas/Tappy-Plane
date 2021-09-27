37071aadf44f1fcf15c68eced5359600df7069f9
Tomas <agu99tomas@gmail.com>
2021-09-27 19:12:22 -0300

    first commit

---
+++ game.js
@@ -0,0 +1,197 @@
+// Globar Game Var
+let game;
+
+class StageBackground extends Stage {
+
+    drawRock(ctx, canvas, frame, objects) {
+        ctx.drawImage(
+            objects.rock.image,
+            objects.rock.x + canvas.width,
+            canvas.height - objects.rock.image.height,
+        );
+        objects.rock.x -= 4;
+
+
+        if (objects.rock.x == (canvas.width * -1) - objects.rock.image.width)
+            objects.rock.x = 0;
+
+        // Rock Down
+        ctx.drawImage(
+            objects.rockDown.image,
+            objects.rockDown.x + canvas.width,
+            objects.rockDown.y - 100,
+        );
+        objects.rockDown.x -= 4;
+
+        if (objects.rockDown.x == (canvas.width * -1) - objects.rockDown.image.width)
+            objects.rockDown.x = 0;
+    }
+
+    drawGround(ctx, canvas, frame, objects) {
+        ctx.drawImage(
+            objects.ground.image,
+            objects.ground.x,
+            canvas.height - objects.ground.image.height
+        );
+        ctx.drawImage(
+            objects.ground.image,
+            objects.ground.x +
+            objects.ground.image.width,
+            canvas.height - objects.ground.image.height
+        );
+
+        objects.ground.x -= 4;
+        if (objects.ground.x == objects.ground.image.width * -1)
+            objects.ground.x = 0;
+    }
+
+    drawBackground(ctx, canvas, frame, objects) {
+        ctx.drawImage(
+            objects.background.image,
+            objects.background.x,
+            objects.background.y
+        );
+        ctx.drawImage(
+            objects.background.image,
+            objects.background.x +
+            objects.background.image.width,
+            objects.background.y
+        );
+
+        objects.background.x -= 1;
+        if (objects.background.x == objects.background.image.width * -1)
+            objects.background.x = 0;
+    }
+
+}
+
+class StageGetReady extends Stage {
+
+    constructor() {
+        super()
+        this.background = new StageBackground();
+        this.nextStage = false;
+        this.canNextStage = false;
+        this.alpha = 1;
+
+        Events.addEventListener(Events.clickOnCanvas, e => {
+            this.nextStage = true;
+        });
+    }
+
+    loading(ctx, canvas, loadedImage, totalImages) { }
+
+    start(canvas, objects) {
+
+        // text ready
+        objects.textGetReady.x = (canvas.width / 2) - objects.textGetReady.image.width / 2
+        objects.textGetReady.y = 100
+
+        // Tap right
+        objects.tapRight.x = (canvas.width / 2) - objects.tapRight.image.width / 2;
+        objects.tapRight.y = 300
+
+        // plane
+        objects.planeYellow.y = objects.planeYellow.y = (canvas.height / 2) - objects.planeYellow.image.height / 2;;
+        objects.planeYellow.x = 100;
+
+    }
+
+    draw(ctx, canvas, frame, objects) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        this.background.drawBackground(ctx, canvas, frame, objects);
+        this.background.drawGround(ctx, canvas, frame, objects);
+
+        ctx.drawImage(objects.planeYellow.image, objects.planeYellow.x, objects.planeYellow.y);
+        objects.planeYellow.animation(frame)
+        
+        if (this.nextStage) {
+            if (this.alpha > 0)  this.alpha -= 0.04;
+            
+            if (this.alpha < 0) {
+                this.alpha = 0;
+                this.canNextStage = true;
+            }
+            ctx.globalAlpha = this.alpha;
+        }
+
+        ctx.drawImage(objects.textGetReady.image, objects.textGetReady.x, objects.textGetReady.y);
+        ctx.drawImage(objects.tapRight.image, objects.tapRight.x, objects.tapRight.y);
+        ctx.globalAlpha = 1.0;
+
+        if (this.canNextStage) game.setStage(new StagePlay());
+
+    }
+
+}
+
+class StagePlay extends Stage {
+
+    constructor() {
+        super();
+        this.background = new StageBackground();
+        this.drop = -2;
+
+        Events.addEventListener(Events.clickOnCanvas, e => {
+            if (this.drop < 0) 
+                this.drop += 7;
+        });
+    }
+
+    start(canvas, objects) {}
+
+    draw(ctx, canvas, frame, objects) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        this.background.drawBackground(ctx, canvas, frame, objects);
+        this.background.drawRock(ctx, canvas, frame, objects);
+        this.background.drawGround(ctx, canvas, frame, objects);
+
+        ctx.drawImage(objects.planeYellow.image, objects.planeYellow.x, objects.planeYellow.y);
+        objects.planeYellow.animation(frame)
+
+        objects.planeYellow.y -= this.drop;
+        this.drop -= 0.2;
+
+        /*ctx.save();
+        ctx.translate(objects.planeYellow.x, objects.planeYellow.y);
+        ctx.rotate(-10);
+        ctx.translate(-objects.planeYellow.x,-objects.planeYellow.y);
+        ctx.drawImage(objects.planeYellow.image, objects.planeYellow.x, objects.planeYellow.y);
+        ctx.restore();*/
+
+    }
+
+}
+
+
+window.onload = () => {
+    let stageGetReady = new StageGetReady();
+    game = new Game(stageGetReady);
+
+    // Background
+    let gameObjectBackground = new StaticGameObject('background', 'background.png');
+    game.addGameObject(gameObjectBackground);
+
+    // Ground
+    let gameObjectGround = new StaticGameObject('ground', 'groundGrass.png');
+    game.addGameObject(gameObjectGround);
+
+    // Rocks
+    let gameObjectRock = new StaticGameObject('rock', 'rockGrass.png');
+    game.addGameObject(gameObjectRock);
+
+    let gameObjectRockDown = new StaticGameObject('rockDown', 'rockGrassDown.png');
+    game.addGameObject(gameObjectRockDown);
+
+    // UI
+    let gameObjectTextGetReady = new StaticGameObject('textGetReady', 'textGetReady.png');
+    game.addGameObject(gameObjectTextGetReady);
+
+    let gameObjectTapRight = new StaticGameObject('tapRight', 'tapRight.png');
+    game.addGameObject(gameObjectTapRight);
+
+    // Game
+    let gameImagesPlaneYellow = ['planeYellow1.png', 'planeYellow2.png', 'planeYellow3.png'];
+    let gameObjectPlaneYellow = new AnimatedGameObject('planeYellow', gameImagesPlaneYellow, 2);
+    game.addGameObject(gameObjectPlaneYellow);
+};
---
+++ motorGame.js
@@ -0,0 +1,162 @@
+
+// Useful Classes
+class Random {
+    static randomInt(min, max) { // min and max included 
+        return Math.floor(Math.random() * (max - min + 1) + min)
+    }
+}
+
+// Game-wide Events and Resources
+
+class Resources {
+    static resourcesPath = "./resources/";
+    static loadedImages = 0;
+    static totalImages = 0;
+
+    static loadImage(imageFileName) {
+        Resources.totalImages++;
+
+        let image = new Image();
+        image.src = "./resources/" + imageFileName;
+        image.onload = e => {
+            Resources.loadedImages++;
+            Events.dispatchEvent(Events.loadedImage);
+        }
+        return image;
+    }
+}
+
+class Events {
+    static loadedImage = new Event("loadedImage");
+    static clickOnCanvas = new Event("clickOnCanvas");
+
+    static dispatchEvent(eventName) {
+        document.dispatchEvent(eventName);
+    }
+
+    static addEventListener(event, callable) {
+        document.addEventListener(event.type, e => callable(e), false);
+    }
+
+}
+
+
+// Game Objects
+
+class GameObject {
+
+    constructor(id) {
+        this.id = id;
+        this.x = 0;
+        this.y = 0;
+        this.image = undefined;
+    }
+}
+
+class StaticGameObject extends GameObject {
+
+    constructor(id, imageFileName) {
+        super(id);
+        this.image = Resources.loadImage(imageFileName);
+    }
+}
+
+class AnimatedGameObject extends GameObject {
+
+    constructor(id, imageFileNames, animationSpeed) {
+        super(id);
+
+        this.animationSpeed = animationSpeed;
+        this.images = [];
+
+        this.loadImages(imageFileNames);
+    }
+
+    loadImages(imageFileNames) {
+        self = this;
+        imageFileNames.forEach(function (imageFileName, i) {
+            let image = Resources.loadImage(imageFileName);
+            self.images.push(image);
+
+            if (i == 0) self.image = image;
+        });
+
+    }
+
+    animation(currentFrame) {
+        if (currentFrame % this.animationSpeed == 1) this.nextImage();
+    }
+
+    nextImage() {
+        const currentIndex = this.images.indexOf(this.image);
+        const nextIndex = (currentIndex + 1) % this.images.length;
+        this.image = this.images[nextIndex];
+    }
+}
+
+// Stage base class
+
+class Stage {
+
+    start(canvas, objects) { } // called in the first frame of stage
+
+    draw(ctx, canvas, frame, objects) { } // called frame by frame
+
+    loading(ctx, canvas, loadedImage, totalImages) { } // called when the game loads
+}
+
+// Game Class
+
+class Game {
+
+    constructor(firstStage) {
+        this.canvas = document.getElementById("canvas");
+        this.canvas.width = 800;
+        this.canvas.height = 480;
+        this.ctx = canvas.getContext("2d");
+        this.stage = firstStage;
+        this.firstFrame = true;
+        this.gameObjects = {};
+
+        this.canvas.onclick = e => {
+            Events.dispatchEvent(Events.clickOnCanvas);
+        }
+        Events.addEventListener(Events.loadedImage, e => this.loading());
+    }
+
+    setStage(stage) {
+        this.stage = stage;
+        this.firstFrame = true;
+    }
+
+    addGameObject(gameObject) {
+        this.gameObjects[gameObject.id] = gameObject;
+    }
+
+    loading() {
+        if (Resources.loadedImages == Resources.totalImages)
+            this.start();
+        else
+            this.stage.loading(this.ctx, this.canvas, Resources.loadedImages, Resources.totalImages);
+    }
+
+    drawStage(ctx, canvas, frame) {
+        if (this.firstFrame) {
+            this.stage.start(canvas, this.gameObjects);
+            this.firstFrame = false;
+        } else {
+            this.stage.draw(ctx, canvas, frame, this.gameObjects);
+        }
+    }
+
+    start() {
+        let frame = 1;
+
+        setInterval(() => {
+            if (frame == 60) frame = 1;
+            this.drawStage(this.ctx, this.canvas, frame);
+            frame++;
+        }, 16.66666666666667);
+
+    }
+}

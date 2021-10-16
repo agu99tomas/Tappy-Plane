037a9bd261961c2210cbd269a38bd3ba1bc1bbe2
Tomás Agú <agu99tomas@gmail.com>
2021-10-15 22:52:52 -0300

    refactor

--- js/motor.js
+++ js/motor.js
@@ -5,7 +5,6 @@ class Random {
     }
 }
 
-
 class Image2D extends Image {
     static pathToImage = "./resources/";
     static loadedImages = 0;
@@ -28,18 +27,19 @@ class BaseObject2D {
     }
 }
 
-
 class Object2D extends BaseObject2D {
     constructor(id, animationSpeed = 2) {
         super(id);
         this.x = 0;
         this.y = 0;
         this.images = [];
+        this.image = undefined;
         this.animationSpeed = animationSpeed;
     }
 
     addImage(image2D) {
         this.images.push(image2D);
+        this.image = this.images[0];
     }
 
     playAnimation(frame) {
@@ -48,27 +48,37 @@ class Object2D extends BaseObject2D {
         }
     }
 
-    hasCollision(){
+    nextImage() {
+        const currentIndex = this.images.indexOf(this.image);
+        const nextIndex = (currentIndex + 1) % this.images.length;
+        this.image = this.images[nextIndex];
+    }
+
+    hasCollision() {
 
     }
-    
+
 }
 
 
-class CollectionObject2D extends BaseObject2D{
+class CollectionObject2D extends BaseObject2D {
 
-    constructor(id, object2D) {
+    constructor(id, baseObject2D = undefined) {
         super(id);
-        this.object2D = object2D;
+        this.object2D = baseObject2D;
         this.objects = [];
     }
 
     duplicate() {
-        let clone = Object.assign({}, this.object2D);
+        let clone = Object.assign({}, this.baseObject2D);
         this.objects.push(clone);
     }
 
-    restore() {
+    addObject2D(object2D) {
+        this.objects.push(object2D);
+    }
+
+    restoreObjects() {
         this.objects = [];
     }
 
@@ -85,4 +95,39 @@ class CollectionObject2D extends BaseObject2D{
         });
     }
 
+}
+
+
+
+class Canvas {
+
+    constructor(id){
+        this.canvas = document.getElementById(id);
+        this.ctx = canvas.getContext("2d");
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
+    }
+
+    clear() {
+        this.ctx.clearRect(0, 0, this.width, this.height);
+    }
+
+
+    draw(baseObject2D) {
+        if (typeof baseObject2D === Object2D) {
+            this.drawObject2D(baseObject2D)
+        }
+
+        if (typeof baseObject2D === CollectionObject2D) {
+            baseObject2D.objects.forEach(object2D => {
+                this.drawObject2D(object2D)
+            });
+        }
+
+    }
+
+    drawObject2D(object2D){
+        this.ctx.drawImage(object2D.image, object2D.x, object2D.y);
+        object2D.playAnimation();
+    }
 }

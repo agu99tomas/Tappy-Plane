ede53dfe69111c0909fb7d408979fa40c60b4d26
Tomás Agú <agu99tomas@gmail.com>
2021-10-16 22:13:46 -0300

    refactor

--- js/motor.js
+++ js/motor.js
@@ -1,227 +1,290 @@
 class Random {
-    static randomInt(min, max) {
-        // min and max included 
-        return Math.floor(Math.random() * (max - min + 1) + min)
-    }
+  static randomInt(minIncluded, maxIncluded) {
+    return Math.floor(
+      Math.random() * (maxIncluded - minIncluded + 1) + minIncluded
+    );
+  }
 }
 
 class Events {
-    static loadedImage = new Event("loadedImage");
-    static clickOnCanvas = new Event("clickOnCanvas");
+  static loadedImage = new Event("loadedImage");
 
-    static dispatchEvent(eventName) {
-        document.dispatchEvent(eventName);
+  static dispatchEvent(event, data = undefined) {
+    if (data !== undefined) {
+      event.data = data;
     }
+    document.dispatchEvent(event);
+  }
 
-    static addEventListener(event, callable) {
-        document.addEventListener(event.type, e => callable(e), false);
-    }
-
+  static addEventListener(event, callable) {
+    document.addEventListener(event.type, (e) => callable(e), false);
+  }
 }
 
 class Image2D extends Image {
-    static pathToImage = "./images/";
-    static loadedImages = 0;
-    static totalImages = 0;
+  static pathToImage = "./images/";
+  static loadedImages = 0;
+  static totalImages = 0;
 
-    constructor(imageFileName) {
-        super();
-        Image2D.totalImages++;
+  constructor(imageFileName) {
+    super();
+    Image2D.totalImages++;
 
-        this.src = Image2D.pathToImage + imageFileName;
-        this.onload = e => {
-            Image2D.loadedImages++;
-            Events.dispatchEvent(Events.loadedImage);
-        }
-    }
+    this.src = Image2D.pathToImage + imageFileName;
+    this.onload = (e) => {
+      Image2D.loadedImages++;
+      Events.dispatchEvent(Events.loadedImage);
+    };
+  }
 }
 
-class BaseObject2D {
-    constructor(id) {
-        this.id = id;
+class Object2D {
+  constructor(id, animationSpeed = 2) {
+    this.id = id;
+    this.x = 0;
+    this.y = 0;
+    this.images = [];
+    this.currentImage = undefined;
+    this.animationSpeed = animationSpeed;
+  }
+
+  draw(canvas) {
+    canvas.ctx.drawImage(this.currentImage, this.x, this.y);
+    this.playAnimation(canvas.frame);
+  }
+
+  addImage(imageFileName) {
+    let newImage = new Image2D(imageFileName);
+    this.images.push(newImage);
+    if (this.currentImage === undefined) {
+      this.currentImage = newImage;
     }
+  }
+
+  playAnimation(frame) {
+    if (frame % this.animationSpeed == 0 && this.images.length != 1) {
+      this.nextImage();
+    }
+  }
+
+  nextImage() {
+    const currentIndex = this.images.indexOf(this.currentImage);
+    const nextIndex = (currentIndex + 1) % this.images.length;
+    this.currentImage = this.images[nextIndex];
+  }
+
+  centerY(canvas) {
+    this.y = canvas.height / 2 - this.currentImage.height / 2;
+  }
+
+  centerX(canvas) {
+    this.x = canvas.width / 2 - this.currentImage.width / 2;
+  }
+
+  hasCollision() {}
 }
 
-class Object2D extends BaseObject2D {
-    constructor(id, animationSpeed = 2) {
-        super(id);
-        this.x = 0;
-        this.y = 0;
-        this.images = [];
-        this.currentImage = undefined;
-        this.animationSpeed = animationSpeed;
+class Collection {
+  constructor(id, object) {
+    this.id = id;
+    this.object = object;
+    this.objects = [];
+  }
+
+  draw(canvas) {
+    this.objects.forEach((obj) => {
+      obj.draw(canvas);
+    });
+  }
+
+  addObject() {
+    let clone = Object.assign(this.object);
+    this.objects.push(clone);
+  }
+
+  clearObjects() {
+    this.objects = [];
+  }
+
+  remove(gameObject) {
+    const index = this.objects.indexOf(gameObject);
+    if (index !== -1) {
+      this.objects.splice(index, 1);
     }
+  }
 
-    draw(canvas) {
-        canvas.ctx.drawImage(thus.currentImage, this.x, this.y);
-        this.playAnimation(canvas.frame);
-    }
-
-    addImage(image2D) {
-        this.images.push(image2D);
-        this.currentImage = this.images[0];
-    }
-
-    playAnimation(frame) {
-        if ((frame % this.animationSpeed == 0) && (this.images.length != 1)) {
-            this.nextImage();
-        }
-    }
-
-    nextImage() {
-        const currentIndex = this.images.indexOf(this.currentImage);
-        const nextIndex = (currentIndex + 1) % this.images.length;
-        this.currentImage = this.images[nextIndex];
-    }
-
-    hasCollision() {
-
-    }
-
-}
-
-class CollectionObject2D extends BaseObject2D {
-
-    constructor(id, object2D = undefined) {
-        super(id);
-        this.object2D = object2D;
-        this.objects = [];
-    }
-
-    drawAllObjects2D(canvas) {
-        this.objects.forEach(object2D => {
-            object2D.draw(canvas);
-        });
-    }
-
-    duplicate() {
-        let clone = Object.assign({}, this.object2D);
-        this.objects.push(clone);
-    }
-
-    addObject2D(object2D) {
-        this.objects.push(object2D);
-    }
-
-    restoreObjects() {
-        this.objects = [];
-    }
-
-    remove(gameObject) {
-        const index = this.objects.indexOf(gameObject);
-        if (index !== -1) {
-            this.objects.splice(index, 1);
-        }
-    }
-
-    removeFromList(gameObjects) {
-        gameObjects.forEach(gameObject => {
-            this.remove(gameObject);
-        });
-    }
-
+  removeFromList(objects) {
+    objects.forEach((obj) => {
+      this.remove(obj);
+    });
+  }
 }
 
 class Canvas {
+  constructor(id, width, height) {
+    this.realCanvas = document.getElementById(id);
+    this.ctx = this.realCanvas.getContext("2d");
+    this.realCanvas.width = width;
+    this.realCanvas.height = height;
+    this.width = this.realCanvas.width;
+    this.height = this.realCanvas.height;
+    this.frame = 1;
+  }
 
-    constructor(id, width, height) {
-        this.realCanvas = document.getElementById(id);
-        this.ctx = realCanvas.getContext("2d");
-        this.realCanvas.width = width;
-        this.realCanvas.height = height;
-        this.width = this.realCanvas.width;
-        this.height = this.realCanvas.height;
-        this.frame = 1;
+  clear() {
+    this.ctx.clearRect(0, 0, this.width, this.height);
+  }
+
+  draw(object) {
+    object.draw(this);
+  }
+
+  updateFrame() {
+    if (this.frame == 60) {
+      this.frame = 1;
+    } else {
+      this.frame++;
     }
-
-    clear() {
-        this.ctx.clearRect(0, 0, this.width, this.height);
-    }
-
-    draw(baseObject2D) {
-        if (typeof baseObject2D === Object2D) {
-            baseObject2D.draw(this);
-        }
-
-        if (typeof baseObject2D === CollectionObject2D) {
-            baseObject2D.drawAllObjects2D(this);
-        }
-    }
-
-    updateFrame() {
-        if (this.frame == 61) {
-            this.frame = 1;
-        } else {
-            this.frame++;
-        }
-    }
-
+  }
 }
 
 class Layer {
-    loop(canvas) { }
-    events(e) { }
+  start(canvas) {}
+
+  loop(canvas) {}
+
+  events(e) {}
+
+  addObjects(objects) {
+    objects.forEach((obj) => {
+      Object.defineProperty(this, obj.id, {
+        value: obj,
+      });
+    });
+  }
+
+  setStage(id){
+    Events.dispatchEvent(Events.changeStage)
+  }
+  
 }
 
+class Background extends Layer {
+  constructor(imageFileName, velocity, x = 0, y = 0) {
+    super();
+    this.image = new Image2D(imageFileName);
+    this.velocity = velocity;
+    this.x = x;
+    this.y = y;
+  }
+
+  loop(canvas) {
+    canvas.ctx.drawImage(this.image, this.x, canvas.height - this.image.height);
+    canvas.ctx.drawImage(
+      this.image,
+      this.x + this.image.width,
+      canvas.height - this.image.height
+    );
+
+    this.x -= this.velocity;
+    if (this.x == this.image.width * -1) {
+      this.x = 0;
+    }
+  }
+}
 
 class Stage {
-    constructor(id) {
-        this.id = id;
-        this.layers = [];
-    }
+  constructor(id) {
+    this.id = id;
+    this.layers = [];
+  }
 
-    addLayer(layer) {
-        this.layers.push(layer);
-    }
+  addLayer(layer) {
+    this.layers.push(layer);
+  }
 
-    drawLayers(canvas) {
-        this.layers.forEach(layer => {
-            layer.loop(canvas);
-        });
-    }
+  drawLayers(canvas) {
+    this.layers.forEach((layer) => {
+      layer.loop(canvas);
+    });
+  }
 
-    notifyLayersEvent(e){
+  notifyLayersEvent(e) {
+    this.layers.forEach((layer) => {
+      layer.events(e);
+    });
+  }
 
-    }
+  addObjects(objects) {
+    this.layers.forEach((layer) => {
+      layer.addObjects(objects);
+    });
+  }
+
+  start(canvas) {
+    this.layers.forEach((layer) => {
+      layer.start(canvas);
+    });
+  }
 }
 
 class Game {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.stage = undefined;
+    this.stages = {};
+    this.objects = [];
+    this.stageChanged = true;
+  }
 
-    constructor(canvas) {
-        this.canvas = canvas;
-        this.stage = undefined;
-        this.stages = {};
+  addStage(stage) {
+    this.stages[stage.id] = stage;
+
+    let objectsClone = Object.assign(this.objects);
+    stage.addObjects(objectsClone);
+  }
+
+  setStage(id) {
+    let stageClone = Object.assign(this.stages[id]);
+    this.stage = stageClone;
+    this.stageChanged = true;
+  }
+
+  addObject(object) {
+    this.objects.push(object);
+  }
+
+  start() {
+    Events.addEventListener(Events.loadedImage, (e) => {
+      this.loop();
+    });
+  }
+
+  loop() {
+    if (Image2D.loadedImages == Image2D.totalImages) {
+      this.manageEvents();
+
+      const sixtyFPS = 16.66666666666667;
+      setInterval(() => {
+        if (this.stageChanged) {
+          this.stage.start(this.canvas);
+          this.stageChanged = false;
+        }
+        this.canvas.clear();
+        this.stage.drawLayers(this.canvas);
+        this.canvas.updateFrame();
+      }, sixtyFPS);
+    } else {
     }
+  }
 
-    addStage(stage) {
-        this.stages[stage.id] = stage;
-    }
+  manageEvents() {
+    this.canvas.realCanvas.addEventListener("click", (e) => {
+      this.stage.notifyLayersEvent(e);
+    });
 
-    setStage(id) {
-        this.stage = this.stages[id];
-    }
-
-    addGameObject(gameObject) {
-        Object.defineProperty(this, gameObject.id, {
-            value: gameObject
-        });
-    }
-
-    loop() {
-        const sixtyFPS = 16.66666666666667;
-        setInterval(() => {
-            this.stage.drawLayers(this.canvas);
-            this.canvas.updateFrame();
-        }, sixtyFPS);
-    }
-
-    manageEvents() {
-        this.canvas.realCanvas.onclick = e => { Events.dispatchEvent(Events.clickOnCanvas) };
-        Events.addEventListener(Events.loadedImage, e => this.loading());
-
-        Events.addEventListener(Events.clickOnCanvas, e => {
-            this.stage.notifyLayersEvent(e);
-        });
-    }
-
+    document.addEventListener("keydown", (e) => {
+      this.stage.notifyLayersEvent(e);
+    });
+  }
 }
